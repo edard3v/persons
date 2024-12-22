3efb7edeb9c345a211f254505095a4b0febf14ff
edard3v <edard3v@gmail.com>
2024-12-22 02:48:10 -0500

    agg astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   output: "static",
   adapter: cloudflare(),
 });

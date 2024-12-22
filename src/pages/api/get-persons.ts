c04beb82523666edf2c400a8409edab8348ac839
edard3v <edard3v@gmail.com>
2024-12-22 14:41:44 -0500

    paginando

--- src/pages/api/get-persons.ts
+++ src/pages/api/get-persons.ts
@@ -1,13 +1,35 @@
 import type { APIRoute } from "astro";
-import { db, Persons } from "astro:db";
+import { count, db, Persons } from "astro:db";
 
 export const prerender = false;
 
-export const GET: APIRoute = async () => {
-  const persons = await db.select().from(Persons);
+export const GET: APIRoute = async ({ request }) => {
+  const url = new URL(request.url);
 
-  return new Response(JSON.stringify(persons), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+  let page = Number(url.searchParams.get("page"));
+  if (!page) page = 1;
+
+  const limit = 6;
+
+  const [records] = await db.select({ count: count() }).from(Persons);
+  const totalPages = Math.ceil(records.count / limit) || 1;
+
+  const persons = await db
+    .select()
+    .from(Persons)
+    .limit(limit)
+    .offset((page - 1) * limit);
+
+  return new Response(
+    JSON.stringify({
+      page,
+      limit,
+      totalPages,
+      records: persons,
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 };

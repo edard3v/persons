03e64b19a6b597041293b3494f1e4223fd07f918
edard3v <edard3v@gmail.com>
2024-12-22 00:15:24 -0500

    agg adaptador cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
